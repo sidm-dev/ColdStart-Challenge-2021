3bd9c10eb05daa7332736eb6ebf2de46987f2b64
sidm <sidm@blueyonder.co.uk>
2021-03-05 12:59:22 +0000

    more h1

--- challenges/challenge1/starter/coldstartapp/vue-app/src/views/catalog/catalog-list.vue
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/views/catalog/catalog-list.vue
@@ -38,7 +38,7 @@ export default {
       Loading data ...
     </div>
     <div class="container">
-      <h1>{{user.userDetails}}</h1>
+      <h1>xxxxxxxxxx{{user.userDetails}}</h1>
       <AuthLogin provider="google">Login using Google</AuthLogin>
       <AuthLogout>Logout</AuthLogout>
       <div
