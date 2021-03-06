75c47e1c2b50a3f667e8386b7a4d253f0b55165f
sidm <sidm@blueyonder.co.uk>
2021-03-06 07:29:11 +0000

    Copied Auth-Login to Navbar

--- challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
@@ -1,4 +1,5 @@
 <script>
+import { mapGetters } from 'vuex';
 import HeaderBarBrand from '@/components/header-bar-brand.vue';
 
 export default {
@@ -9,6 +10,9 @@ export default {
   data() {
     return {
     };
+  },
+  computed: {
+    ...mapGetters('user', { user: 'user' }),
   },
   methods: {
   },
@@ -22,6 +26,14 @@ export default {
       <div class="navbar-menu">
         <div class="navbar-start">
           <router-link class="navbar-item nav-home" to="/">Home</router-link>
+           <div v-if="!this.user">
+            <AuthLogin provider="aad"></AuthLogin>
+            <AuthLogin provider="facebook"></AuthLogin>
+            <AuthLogin provider="github"></AuthLogin>
+            <AuthLogin provider="google"></AuthLogin>
+            <AuthLogin provider="twitter"></AuthLogin>
+          </div>
+          <AuthLogout v-if="this.user">Logout</AuthLogout>
         </div>
       </div>
     </nav>
